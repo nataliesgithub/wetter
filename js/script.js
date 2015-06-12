507acc94c9fefde923f6290999ea290e6bdfc2e7
nataliesgithub <littlesistha_90@hotmail.com>
2015-06-12 11:00:08 +0200

    modify js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
 			latitude:position.coords.latitude
 		};
 
+		//API ANRFAGE:
 		$.ajax({ //Promise Callback: spezielle Objekte, kann erst ausgeführt werden, wenn Methode bereit ist
 
-			//API Request, um zu intentifizieren
 			url: 'https://api.forecast.io/forecast/9913fe3fb0ae16c65896c6408574aa2d/' + koordinaten.latitude + ',' + koordinaten.longitude,
 			
 			data: {
@@ -19,7 +19,7 @@ $(document).ready(function(){
 				lang: 'de'
 			},
 
-			//jasonP deklarieren, API-Abfrage
+			//jasonP deklarieren, API-Abfrage (Intentifikation)
 			dataType : 'jsonp'
 
 
@@ -28,7 +28,7 @@ $(document).ready(function(){
 				console.log(data);
 				$("#currentTemp").text(data.currently.apparentTemperature + "°C");
 				//$("#source").text(data.flags['metno-license']); 
-				//Wenn ein Sonderzeichen im Key vorkommt, muss dieser mit eckigen Klammern geschrieben und kann nicht mehr via Punkt-Notation angesteuert werden
+				//Sonderzeichen im Key: in eckigen Klammern geschrieben und kann nicht mehr via Punkt-Notation angesteuert werden
 				
 				//GOOGLE GEO-CODING ANFRAGE:
 				$.ajax({
