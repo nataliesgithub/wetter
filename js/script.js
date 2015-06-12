27ce6e993d368e0d2e30292af4d93ba7fbf43067
nataliesgithub <littlesistha_90@hotmail.com>
2015-06-12 09:17:19 +0200

    modify script

--- js/script.js
+++ js/script.js
@@ -10,22 +10,23 @@ $(document).ready(function(){
 		$.ajax({ //Promise Callback: spezielle Objekte, kann erst ausgeführt werden, wenn Methode bereit ist
 
 			//API Request, um zu intentifizieren
-			url: 'https:api.forecast.io/forecast/9913fe3fb0ae16c65896c6408574aa2d' + koordinaten.latitude + ',' + koordinaten.longitude,
+			url: 'https://api.forecast.io/forecast/9913fe3fb0ae16c65896c6408574aa2d' + koordinaten.latitude + ',' + koordinaten.longitude,
 			
 			data: {
+
 				//?units=si&lang=de in php z.b.
 				units: 'si',
 				lang: 'de'
 			},
 
-
 			//jasonP deklarieren, API-Abfrage
 			dataType : 'jsonp'
+
 
 		}).done(function(data) {
 				
 				console.log(data);
-				
+
 		});
 
 	});
